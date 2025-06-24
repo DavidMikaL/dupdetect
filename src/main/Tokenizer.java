8288edd9e106dc882e7125461519656abcf705c2
khimportiert <khimpaully@gmail.com>
2025-06-24 21:07:53 +0200

    tokenizer

---
+++ src/main/Tokenizer.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Tokenizer {
+    private static final byte[] LOOKUP_TABLE = new byte[256];
+
+    public Tokenizer() { }
+
+    static {
+        for (int i = 0; i < 256; i++) {
+            if (i >= 'a' && i <= 'z') {
+                LOOKUP_TABLE[i] = (byte) i;
+            } else if (i >= 'A' && i <= 'Z') {
+                LOOKUP_TABLE[i] = (byte) (i + 0x20);
+            } else if (i >= '0' && i <= '9') {
+                LOOKUP_TABLE[i] = (byte) (i);
+            } else if (i == '.' || i == '-' || i == '/') {
+                LOOKUP_TABLE[i] = (byte) (i);
+            } else {
+                LOOKUP_TABLE[i] = ' ';
+            }
+        }
+    }
+
+    private String sanitize(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            sb.append((char) LOOKUP_TABLE[c & 0xFF]);
+        }
+        return sb.toString();
+    }
+
+    public ArrayList<String> tokenize(String input) {
+        ArrayList<String> tokens = new ArrayList<>();
+        for (String s : sanitize(input).split("\\s+")) {
+            tokens.add(s);
+        }
+        return tokens;
+    }
+}
